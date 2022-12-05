7e656a8d5ab23954aadd321b15b04383eee663f2
Damian Soriano <damian.soriano19@gmail.com>
2022-12-05 13:06:24 -0300

    Agregué un login y mejoré el fuction cuotas

--- Js/main.js
+++ Js/main.js
@@ -1,19 +1,14 @@
-/*let usuario = prompt ("Ingrese su Usuario")
-let contraseña = prompt ("ingrese su contraseña")
-    
-if (usuario == "Damian" && contraseña == "1234") {
-    alert ("Bienvenido DamianSoriano")
-        console.log ("Bienvenido Damian")
-    } 
-    
-    else{
-        if (usuario != "Damian" && contraseña != "1234"){
-            alert ("Usuario o contraseña incorrecto")
-            console.log ("Usuario o Contraseña incorrecto")
-        }
-    }
-*/
+//Login de mi página
 for (i = 1 ; i < 4 ; i++) { 
+    let usuario = prompt ("Ingrese su Usuario");
+    let contraseña = prompt ("ingrese su contraseña");
+    if ((usuario == "Damian") && (contraseña == "1234")) {
+        alert ("Bienvenido DamianSoriano");
+            console.log ("Bienvenido Damian");
+            break;
+        } else {
+            alert ("Usuario o contraseña incorrecto")
+        } 
 
 }
 
@@ -24,6 +19,8 @@ let cuota = parseInt (prompt ("ingresa valor cuota (0 para salir)"));
 function cuotas (precio , cuota) {
     precioCuotas = precio / cuota;
     alert ("el precio en "+ cuota + " cuotas es de $" + precioCuotas);
+    console.log ("El valor del producto ingresado es de " + precio )
+    console.log ("el precio en "+ cuota + " cuotas es de $" + precioCuotas)
 }
 
 
